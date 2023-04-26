8bef88fcfd190962ff24a9894b837fbd4a189efd
neonilakunyk <n.kunyk-2025@kpi.ua>
2023-04-26 10:29:53 +0300

    ST-6: * fixed handleError

--- frontend/src/helpers/exception/handle-error/handle-error.ts
+++ frontend/src/helpers/exception/handle-error/handle-error.ts
@@ -8,7 +8,7 @@ const handleError = (
 ): void => {
   const isHttpError = error instanceof HttpError;
   const message = isHttpError ? error.message : EXTERNAL_SERVER_ERROR_MESSAGE;
-  showError(message);
+  showError(message as HttpErrorMessage);
 };
 
 export { handleError };
