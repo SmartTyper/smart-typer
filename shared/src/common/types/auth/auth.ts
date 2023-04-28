f0a8d37a8dd91f9c3ca96b0d14b61027b6b24ea7
neonilakunyk <n.kunyk-2025@kpi.ua>
2023-04-28 11:25:20 +0300

    Fixed auth folders naming

--- shared/src/common/types/auth/auth.ts
+++ shared/src/common/types/auth/auth.ts
@@ -1,5 +1,5 @@
-export * from './register-user-request-dto/register-user-request-dto.type';
-export * from './log-in-user-request-dto/log-in-user-request-dto.type';
+export * from './register-request-dto/register-request-dto.type';
+export * from './log-in-request-dto/log-in-request-dto.type';
 export * from './reset-password-request-dto/reset-password-request-dto.type';
 export * from './set-password-request-dto/set-password-request-dto.type';
 export * from './google-log-in-url-response-dto/google-log-in-url-response-dto.type';
