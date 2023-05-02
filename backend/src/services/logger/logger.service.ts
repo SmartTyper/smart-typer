c047b1e663994cfb3ef6056b2c4f4661d626b8f4
neonilakunyk <n.kunyk-2025@kpi.ua>
2023-05-02 14:55:04 +0300

    finished logger service

--- backend/src/services/logger/logger.service.ts
+++ backend/src/services/logger/logger.service.ts
@@ -1,35 +1,33 @@
-import {
-  Logger as AppLogger,
-  // MessageLogger,
-  // ObjectLogger,
-} from 'common/types/types';
+import { Logger as AppLogger } from 'common/types/types';
 
 class Logger {
   private _logger: AppLogger | null = null;
 
-  public initLogger(logger: AppLogger): void {
-    this._logger = logger;
-  }
-
-  public info: AppLogger['info'] = (...args) => {
+  public info = (
+    ...args: Parameters<AppLogger['info']>
+  ): ReturnType<AppLogger['info']> | void => {
     if (!this._logger) {
       return;
     }
     this._logger.info(...args);
   };
 
-  public warn: AppLogger['warn'] = (args) => {
+  public warn = (
+    ...args: Parameters<AppLogger['warn']>
+  ): ReturnType<AppLogger['warn']> | void => {
     if (!this._logger) {
       return;
     }
-    this._logger.warn(args);
+    this._logger.warn(...args);
   };
 
-  public error: AppLogger['error'] = (args) => {
+  public error = (
+    ...args: Parameters<AppLogger['error']>
+  ): ReturnType<AppLogger['error']> | void => {
     if (!this._logger) {
       return;
     }
-    this._logger.error(args);
+    this._logger.error(...args);
   };
 }
 
