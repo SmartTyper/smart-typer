3cbbb28282c2c723f6e8170f324c1bc470292573
SomMariya <m.som-2030@kpi.ua>
2023-05-28 05:51:22 +0300

    clean logs

--- backend/src/services/user/user.service.ts
+++ backend/src/services/user/user.service.ts
@@ -84,8 +84,6 @@ class User {
 
   public async getByEmail(email: string): Promise<UserWithPassword> {
     const user = await this._userRepository.getByEmail(email);
-    console.log(user);
-
     if (!user) {
       throw new HttpError({
         status: HttpCode.NOT_FOUND,
