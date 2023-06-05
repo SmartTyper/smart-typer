5de67aacae6491a22f70d2e14d5fbbef4a8ee4bc
SomMariya <m.som-2030@kpi.ua>
2023-06-05 23:01:59 +0300

    added ar.js

---
+++ frontend/src/components/ar-camera/ar-camera.tsx
@@ -0,0 +1,16 @@
+import { FC } from 'common/types/types';
+
+const ArCamera: FC = () => {
+  return (
+    //   <a-scene embedded arjs>
+    //   <a-entity
+    //         obj-model=”obj: url(/path/to/nameOfFile.obj);
+    //         mtl: url(/path/to/nameOfFile.mtl)”>
+    //     </a-entity>
+    //     <a-marker-camera preset="hiro"></a-marker-camera>
+    //   </a-scene>
+    <></>
+  );
+};
+
+export { ArCamera };
